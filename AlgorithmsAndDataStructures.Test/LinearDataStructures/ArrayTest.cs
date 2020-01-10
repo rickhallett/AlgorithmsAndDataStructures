9843f2df671d19a6fbf425cd251feba56734b143
richardhallett <rickhallett@live.co.uk>
2020-01-10 06:13:21 +0000

    get min array function to practice with xUnit

--- AlgorithmsAndDataStructures.Test/LinearDataStructures/ArrayTest.cs
+++ AlgorithmsAndDataStructures.Test/LinearDataStructures/ArrayTest.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using Xunit;
 using AlgorithmsAndDataStructures.LinearDataStructures;
 
@@ -5,12 +6,27 @@ namespace AlgorithmsAndDataStructures.Test.LinearDataStructures
 {
     public class ArrayTest
     {
-        [Fact]
-        public void ReturnNumReturnsNum()
+        public Arrays arrays;
+        public ArrayTest()
         {
-            var arrays = new Arrays();
+            arrays = new Arrays();
+        }
+        
+        [Fact]
+        public void FindMinArrayTest()
+        {
+            Assert.Equal(1, arrays.FindMinArray(new []{1, 2, 3}));
+            Assert.Equal(1, arrays.FindMinArray(new []{4, 6, 8, 1, 9, 2}));
+        }
 
-            Assert.Equal(3, arrays.ReturnNum(3));
+        [Theory]
+        [InlineData(new []{1, 2, 3}, 1)]
+        [InlineData(new []{1, 2, 3, 4, 5}, 1)]
+        [InlineData(new []{10, 100, 3, 1}, 1)]
+        [InlineData(new []{2, 6, 4, 1, 100}, 1)]
+        public void FindMinArray2Test(int[] testValue, int expectedValue)
+        {
+            Assert.Equal(expectedValue, arrays.FindMinArray(testValue));
         }
     }
 }

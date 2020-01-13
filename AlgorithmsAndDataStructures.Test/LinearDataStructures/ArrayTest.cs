c84ac11ce14d213ee16be1e0cc0b9b2ca82e4056
richardhallett <rickhallett@live.co.uk>
2020-01-13 22:37:40 +0000

    began implementation of DoublyLinkedList

--- AlgorithmsAndDataStructures.Test/LinearDataStructures/ArrayTest.cs
+++ AlgorithmsAndDataStructures.Test/LinearDataStructures/ArrayTest.cs
@@ -6,27 +6,30 @@ namespace AlgorithmsAndDataStructures.Test.LinearDataStructures
 {
     public class ArrayTest
     {
-        public Arrays arrays;
+        private readonly Arrays _arrays;
+        
         public ArrayTest()
         {
-            arrays = new Arrays();
+            _arrays = new Arrays();
         }
         
-        [Fact]
-        public void FindMinArrayTest()
-        {
-            Assert.Equal(1, arrays.FindMinArray(new []{1, 2, 3}));
-            Assert.Equal(1, arrays.FindMinArray(new []{4, 6, 8, 1, 9, 2}));
-        }
-
         [Theory]
         [InlineData(new []{1, 2, 3}, 1)]
         [InlineData(new []{1, 2, 3, 4, 5}, 1)]
         [InlineData(new []{10, 100, 3, 1}, 1)]
         [InlineData(new []{2, 6, 4, 1, 100}, 1)]
-        public void FindMinArray2Test(int[] testValue, int expectedValue)
+        public void FindMinArrayTest(int[] testValue, int expectedValue)
         {
-            Assert.Equal(expectedValue, arrays.FindMinArray(testValue));
+            Assert.Equal(expectedValue, _arrays.GetMin(testValue));
+        }
+
+        [Theory]
+        [InlineData(new []{1, 2, 3, 4, 5}, false)]
+        [InlineData(new []{1, 2, 2, 4, 5}, true)]
+        [InlineData(new []{5, 5, 5, 5, 5}, true)]
+        public void HasDuplicatesTest(int[] testValues, bool expectedValue)
+        {
+            Assert.Equal(expectedValue, _arrays.HasDuplicates(testValues));
         }
     }
 }
